e2dab5641ec0171b8a790f1898a992092afe6ca0
Patty Rose <oteropatty@gmail.com>
2023-05-21 11:03:34 -0700

    firebase hosting update and fix indentations in indes.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <CssBaseline />
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
